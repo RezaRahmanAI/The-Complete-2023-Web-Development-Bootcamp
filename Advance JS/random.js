02ed2831ec26f08b5c0ec54f3a3b39d0c08176b9
Rizve Rahman Reza <rizverahman.ai@gmail.com>
2023-11-14 15:39:34 +0600

    task done

--- Advance JS/random.js
+++ Advance JS/random.js
@@ -4,3 +4,26 @@ console.log(n);
 var x = n * 6;
 x = Math.floor(x) + 1;
 console.log(x);
+
+
+// var names = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank"];
+// var i = Math.random();
+
+// i = Math.floor(i *6)
+
+// function namePicker(names){
+//     console.log(names[i])
+// }
+
+// namePicker(names)
+
+
+var names = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank"];
+
+function namePicker(names) {
+    var i = Math.floor(Math.random() * names.length);
+    console.log(names[i]);
+}
+
+namePicker(names);
+
