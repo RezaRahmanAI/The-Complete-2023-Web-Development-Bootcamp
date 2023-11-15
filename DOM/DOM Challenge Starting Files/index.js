88bda06342b295221675a6f610efe233dce4c2b8
Rizve Rahman Reza <rizverahman.ai@gmail.com>
2023-11-15 18:48:13 +0600

    initial commit

--- DOM/DOM Challenge Starting Files/index.js
+++ DOM/DOM Challenge Starting Files/index.js
@@ -1,1 +1,12 @@
-document.query
+// document.firstElementChild.lastElementChild.firstElementChild.innerHTML =
+//   'Good Bye';
+
+// var heading = document.firstElementChild.lastElementChild.firstElementChild;
+
+// heading.style.color = 'black';
+
+// //  
+
+// var lstLi = document.getElementsByTagName('li')
+
+// lstLi[2].style.color('purple')
---
+++ Dice Game Project/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,21 @@
+// var img_src = [
+//     './images/dice1.png',
+//     './images/dice2.png',
+//     './images/dice3.png',
+//     './images/dice4.png',
+//     './images/dice5.png',
+//     './images/dice6.png',
+// ];
+
+// var images = [];
+
+// img_src.forEach(src => {
+//     var img = new Image();
+//     img.src = src;
+//     images.push(img);
+// });
+
+// Now, the 'images' array contains HTML Image elements with the specified sources
+
+var img = new Image()
+console.log(img)
