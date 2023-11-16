32598437118fed1c48b987a2755e45cb04a69ef4
Rizve Rahman Reza <rizverahman.ai@gmail.com>
2023-11-16 09:28:31 +0600

    initial commit

--- Dice Game Project/Dicee Challenge - Starting Files/index.js
+++ Dice Game Project/Dicee Challenge - Starting Files/index.js
@@ -1,21 +1,24 @@
-// var img_src = [
-//     './images/dice1.png',
-//     './images/dice2.png',
-//     './images/dice3.png',
-//     './images/dice4.png',
-//     './images/dice5.png',
-//     './images/dice6.png',
-// ];
 
-// var images = [];
+// window.onload = function () {
+//   // Generate random numbers between 1 and 6 for Player 1 and Player 2
+//   var randPlayer1 = Math.floor(Math.random() * 6) + 1;
+//   var randPlayer2 = Math.floor(Math.random() * 6) + 1;
 
-// img_src.forEach(src => {
-//     var img = new Image();
-//     img.src = src;
-//     images.push(img);
-// });
+//   // Set the source of Player 1's image
+//   var player1Image = document.getElementById('player1Image');
+//   player1Image.src = './images/dice' + randPlayer1 + '.png';
 
-// Now, the 'images' array contains HTML Image elements with the specified sources
+//   // Set the source of Player 2's image
+//   var player2Image = document.getElementById('player2Image');
+//   player2Image.src = './images/dice' + randPlayer2 + '.png';
+// };
 
-var img = new Image()
-console.log(img)
+var randomNumber1 = Math.floor(Math.random() * 6) + 1
+var randomImg1 = './images/dice' + randomNumber1 + '.png'
+var img1 = document.querySelectorAll("img")[0]
+img1.setAttribute("src", randomImg1)
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1
+var randomImg2 = './images/dice' + randomNumber2 + '.png'
+var img2 = document.querySelectorAll("img")[1]
+img2.setAttribute('src', randomImg2)
