37eb8ff4ac85e24b261b33a3cf16c651cb08ed00
Rizve Rahman Reza <rizverahman.ai@gmail.com>
2023-11-17 20:59:01 +0600

    Initial commit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,11 +1,10 @@
 var numberOfDrum = document.querySelectorAll('.drum').length
-var audio = new Audio('./sounds/crash.mp3')
 
-// for (var i = 0; i < numberOfDrum; i++){
-//     document
-//   .querySelectorAll('.drum')[i]
-//   .addEventListener('click', () => audio.play())
-// }
+function playAudio(soundFile) {
+  var audio = new Audio(soundFile)
+  audio.play()
+}
+
 
 for (var i = 0; i < numberOfDrum; i++) {
   document.querySelectorAll('.drum')[i].addEventListener('click', function () {
@@ -13,38 +12,32 @@ for (var i = 0; i < numberOfDrum; i++) {
 
     switch (buttonInnerHtml) {
       case 'w':
-        var audio = new Audio('./sounds/tom-1.mp3')
-        audio.play()
+        playAudio('./sounds/tom-1.mp3')
+      
         break
 
       case 'a':
-        var audio = new Audio('./sounds/tom-2.mp3')
-        audio.play()
+        playAudio('./sounds/tom-2.mp3')
         break
 
       case 's':
-        var audio = new Audio('./sounds/tom-3.mp3')
-        audio.play()
+        playAudio('./sounds/tom-3.mp3')
         break
 
       case 'd':
-        var audio = new Audio('./sounds/tom-4.mp3')
-        audio.play()
+        playAudio('./sounds/tom-4.mp3')
         break
 
       case 'j':
-        var audio = new Audio('./sounds/snare.mp3')
-        audio.play()
+        playAudio('./sounds/snare.mp3')
         break
 
       case 'k':
-        var audio = new Audio('./sounds/crash.mp3')
-        audio.play()
+        playAudio('./sounds/crash.mp3')
         break
 
       case 'l':
-        var audio = new Audio('./sounds/kick-bass.mp3')
-        audio.play()
+        playAudio('./sounds/kick-bass.mp3')
         break
 
       default:
