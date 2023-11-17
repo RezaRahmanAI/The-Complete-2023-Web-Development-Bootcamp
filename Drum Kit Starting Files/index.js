5040e9d693e94615d03386104c8939e4c43898cf
Rizve Rahman Reza <rizverahman.ai@gmail.com>
2023-11-17 23:54:51 +0600

    Initial commit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,50 +1,58 @@
-var numberOfDrum = document.querySelectorAll('.drum').length
-
 function playAudio(soundFile) {
   var audio = new Audio(soundFile)
   audio.play()
 }
-
+var numberOfDrum = document.querySelectorAll('.drum').length
 
 for (var i = 0; i < numberOfDrum; i++) {
   document.querySelectorAll('.drum')[i].addEventListener('click', function () {
     var buttonInnerHtml = this.innerHTML
 
-    switch (buttonInnerHtml) {
-      case 'w':
-        playAudio('./sounds/tom-1.mp3')
-      
-        break
+    makeSound(buttonInnerHtml)
 
-      case 'a':
-        playAudio('./sounds/tom-2.mp3')
-        break
-
-      case 's':
-        playAudio('./sounds/tom-3.mp3')
-        break
-
-      case 'd':
-        playAudio('./sounds/tom-4.mp3')
-        break
-
-      case 'j':
-        playAudio('./sounds/snare.mp3')
-        break
-
-      case 'k':
-        playAudio('./sounds/crash.mp3')
-        break
-
-      case 'l':
-        playAudio('./sounds/kick-bass.mp3')
-        break
-
-      default:
-        console.log(buttonInnerHtml)
-        break
-    }
   })
+}
+
+document.addEventListener('keypress', function (event) {
+var keyKeys = event.key.toLowerCase()
+makeSound(keyKeys)
+})
+
+function makeSound(key) {
+  switch (key) {
+    case 'w':
+      playAudio('./sounds/tom-1.mp3')
+
+      break
+
+    case 'a':
+      playAudio('./sounds/tom-2.mp3')
+      break
+
+    case 's':
+      playAudio('./sounds/tom-3.mp3')
+      break
+
+    case 'd':
+      playAudio('./sounds/tom-4.mp3')
+      break
+
+    case 'j':
+      playAudio('./sounds/snare.mp3')
+      break
+
+    case 'k':
+      playAudio('./sounds/crash.mp3')
+      break
+
+    case 'l':
+      playAudio('./sounds/kick-bass.mp3')
+      break
+
+    default:
+      console.log(buttonInnerHtml)
+      break
+  }
 }
 
 // function add(num1, num2) {
