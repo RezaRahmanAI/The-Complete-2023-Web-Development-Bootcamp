150f5a7e0ccb4b016f605a771196ffda4e4907a2
Rizve Rahman Reza <rizverahman.ai@gmail.com>
2023-11-23 15:43:19 +0600

    initial commit

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,1 +1,14 @@
-$('h1').css('color','red')
+$('h1').addClass('big-font')
+
+// $('h1').click(function () {
+//     $('h1').css('color', 'purple')
+// })
+
+
+$('button').click(function () {
+    $('h1').addClass('big-font')
+})
+
+$('input').keypress(function (event) {
+    $('h1').html(event.key)
+})
